3e07295489513e502b394aa25aee06ac7c1449bf
Prashant Kumar <prashantkumar22205@gmail.com>
2026-05-12 21:51:05 +0530

    Remove files from repository

--- server.js
+++
@@ -1,9 +0,0 @@
- require("dotenv").config()
-const app = require("./src/index")
-const ConnectDB = require("./src/config/db")
-
-ConnectDB()
-
-app.listen(3000,()=>{
-    console.log("Server is running !")
-})
--- src/routes/auth.route.js
+++
@@ -1,7 +0,0 @@
-const express= require("express")
-const authController = require("./controller/auth.controller")
-const route = express.Router()
-
-route.post("/register",authController.)
-
-module.exports=route
